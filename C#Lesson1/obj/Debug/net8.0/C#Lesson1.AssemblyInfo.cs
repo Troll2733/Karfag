b2639e0d992016b04cd52ac773ad6ab21a41c79f
Vsevolod <Troll2733@mail.ru>
2024-02-10 21:25:15 +0300

    Задачка2

--- C#Lesson1/obj/Debug/net8.0/C#Lesson1.AssemblyInfo.cs
+++ C#Lesson1/obj/Debug/net8.0/C#Lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3aa84b00c0bc13ef24d50cae4b468a5d0739ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26e94bd1363320f68fd4f44eceeb424f9a7e1b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#Lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
