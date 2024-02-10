b26e94bd1363320f68fd4f44eceeb424f9a7e1b3
Vsevolod <Troll2733@mail.ru>
2024-02-10 12:57:39 +0300

    Доработал

---
+++ C#Lesson1/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.Write("Введите первое число: ");
+int N1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите второе число: ");
+int N2 = Convert.ToInt32(Console.ReadLine());
+
+if (N1 == N2)
+{
+   System.Console.WriteLine(N1 + " = " + N2);
+}
+
+if (N1 > N2)
+{
+   int max = N1;
+   int min = N2;
+
+   System.Console.WriteLine("max = " + N1);
+   System.Console.WriteLine("min = " + N2);
+}
+else
+{
+   int max = N2;
+   int min = N1;
+
+   System.Console.WriteLine("max = " + N2);
+   System.Console.WriteLine("min = " + N1);
+}
--- C#Lesson1/obj/Debug/net8.0/C#Lesson1.AssemblyInfo.cs
+++ C#Lesson1/obj/Debug/net8.0/C#Lesson1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("C#Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3aa84b00c0bc13ef24d50cae4b468a5d0739ac7")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
+[assembly: System.Reflection.AssemblyProductAttribute("C#Lesson1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("C#Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
