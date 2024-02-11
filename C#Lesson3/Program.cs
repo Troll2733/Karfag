4f141b5eb9e0bfb26958107451746bb945cbc4f3
Vsevolod <Troll2733@mail.ru>
2024-02-11 19:04:33 +0300

    Решил 3 задачку

---
+++ C#Lesson3/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+System.Console.WriteLine($"Четное - {N}");
+}
+
+else
+{
+    System.Console.WriteLine($"Нечетное - {N}");
+} 
