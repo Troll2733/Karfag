c594673dccd33e3bdc9de0601319b8211fdb4d2f
Vsevolod <Troll2733@mail.ru>
2024-02-14 18:16:44 +0300

    решил задачку 7

---
+++ C#Lesson7/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.WriteLine("Введите число не больше 99 и не больше 10: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int right = N / 10;
+int left = N % 10;
+if (N <= 99 && N >= 10)
+{
+    if (right > left)
+    {
+        Console.WriteLine($"max ={right}");
+    }
+    else
+    if (right == left)
+    {
+        Console.WriteLine($"Значения равны");
+    }
+    else
+    {
+        Console.WriteLine($"max ={left}");
+    }
+}
+else
+{
+    Console.WriteLine("Введено не корректное значение ");
+}
