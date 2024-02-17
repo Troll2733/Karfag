48ec6da5eeafc9e640c6b6d8f029b4d51d4759eb
Vsevolod <Troll2733@mail.ru>
2024-02-17 14:30:26 +0300

    Решили задачку 8

---
+++ C#Lesson8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+if (N > 9)
+    while (N > 0)
+    {
+        count = N % 10;
+        Console.Write(count + ",");
+        N = N / 10;
+    }
+else
+{
+    Console.WriteLine("Меньше двух цифр");
+}
