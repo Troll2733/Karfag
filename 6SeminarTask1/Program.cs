a8d2d64bda472656166a5f6b8160e623e26c6e1f
Vsevolod <Troll2733@mail.ru>
2024-02-26 17:54:26 +0300

    семинар 6 задачка 1

---
+++ 6SeminarTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,] a =
+{
+{'a','e','c'},
+{'-','<','}'},
+{'h','e','c'}
+};
+string str = "";
+for (int i = 0; i < a.GetLength(0); i++)
+    for (int j = 0; j < a.GetLength(1); j++)
+    {
+        str += a[i,j];
+
+    }
+    System.Console.WriteLine(str);
+
+
+
+
+
+
+
