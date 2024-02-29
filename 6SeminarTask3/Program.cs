657fbfa298ed433477c2b05e9d401fe7bf151081
Vsevolod <Troll2733@mail.ru>
2024-02-29 17:23:19 +0300

    задачка решена

---
+++ 6SeminarTask3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+System.Console.WriteLine("Введите строку: ");
+string? str = Console.ReadLine();
+System.Console.WriteLine(str);
+
+for (int i = 0; i < str!.Length / 2; i++)
+{
+    if (str[i] != str[str.Length - 1 - i])
+    {
+        System.Console.WriteLine("Не является палиндромом");
+        return;
+    }
+    
+    
+}
+System.Console.WriteLine("Является Палиндромом");
+
+
